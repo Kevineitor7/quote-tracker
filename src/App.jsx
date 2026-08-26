a7f917d1e33f4c3056ca6b526639f87b3e58eee3
Kevin <qeven03@gmail.com>
2026-08-26 06:19:53 -0500

    quote tracker

---
+++ src/App.jsx
@@ -0,0 +1,476 @@
+import { useState, useReducer } from 'react'
+
+function quotesReducer(quotes, action) {
+  switch (action.type) {
+    case 'add-job':
+      return [
+        ...quotes,
+        action.payload
+      ]
+    case 'edit-job':
+      return quotes.map((quote) =>
+        quote.id === action.payload.id
+          ? { ...action.payload }
+          : quote
+      )
+    case 'remove-job':
+      return quotes.filter(quote => quote.id !== action.payload)
+    case 'toggle-details':
+      return quotes.map((quote) => 
+          quote.id === action.payload
+            ? { ...quote, details: !quote.details }
+            : quote
+      )
+    case 'status-update':
+      return quotes.map((quote) => 
+        quote.id === action.payload.id
+          ? { ...quote, status: action.payload.status }
+          : quote
+      )
+    default:
+      return quotes
+  }
+}
+
+function getTotal(type, items) {
+  if (type === "Exterior") {
+    let sqft = Number(items[0].sqft)
+    let colors = Number(items[0].colors)
+    let colorChange = items[0].cc
+    let ccRate = 1.2
+
+    let total = (sqft * colors) / 1.4
+    if (colorChange) total *= ccRate 
+    return (Math.round(total / 100) * 100).toFixed(0)
+  } else if (type === "Interior") {
+    let roomRates = {
+      "Bedroom (Walls)": 300,
+      "Bedroom (Walls + Trim)": 400,
+      "Bedroom (Walls + Trim + Ceiling)": 600,
+      "Living Room (Walls)": 400,
+      "Living Room (Walls + Trim)": 500,
+      "Living Room (Walls + Trim + Ceiling)": 700,
+      "Kitchen (Walls)": 300,
+      "Kitchen (Walls + Trim)": 500,
+      "Kitchen (Walls + Trim + Ceiling)": 600,
+    }
+    let total = items.reduce((acc, room) => {
+      acc += roomRates[room.description]
+      if (room.cc) acc *= 1.4
+      return acc
+    }, 0)
+    return (Math.round(total / 100) * 100).toFixed(0)
+  }
+}
+
+function App() {
+
+  const [quoteForm, setQuoteForm] = useState(false)
+  const [formJobType, setformJobType] = useState("Interior")
+  const [form, setForm] = useState({
+    id: crypto.randomUUID(),
+    date: "",
+    name: "", 
+    address: "",
+    type: formJobType,
+    items: [],
+    status: "Draft",
+    details: false,
+    editing: false
+  })
+
+  const [rooms, setRooms] = useState([
+    {type: "Bedroom", name: "Bedroom"},
+    {type: "Kitchen", name: "Kitchen"},
+    {type: "Living Room", name:"Living Room"},
+  ])
+
+  const [quotes, dispatch] = useReducer(quotesReducer, [
+    { 
+      id: crypto.randomUUID(),
+      date: "2026-05-30", 
+      name: "Lester Lucas",
+      address: "Kingorito",
+      type: "Interior",
+      items: [
+        {name: "Bedroom", description: "Bedroom (Walls + Trim)", cc: true},
+        {name: "Bedroom 2", description: "Bedroom (Walls + Trim)", cc: true},
+        {name: "Living Room", description: "Living Room (Walls + Trim)", cc: true}
+      ], 
+      status: "Draft",
+      details: false,
+      editing: false
+    },
+    { 
+      id: crypto.randomUUID(), 
+      date: "2026-06-30", 
+      name: "Chester Cheetos",
+      address: "Congito, Congo",
+      type: "Exterior",
+      items: [
+        {sqft: "2500", colors: "2", cc: true}
+      ], 
+      status: "Accepted",
+      details: false,
+      editing: false
+    }
+  ])
+
+  quotes.map((quote) => {
+    quote.total = getTotal(quote.type, quote.items)
+  })
+
+  function addBedroom(e) {
+    e.preventDefault()
+    const amountOfBedrooms = rooms.filter((room) => room.type === "Bedroom").length
+    setRooms(prev => [...prev, {type: "Bedroom", name: `Bedroom ${amountOfBedrooms + 1}`}])
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (form.editing === true) {
+      dispatch({ type: 'edit-job', payload: form })
+    } else {
+      dispatch({ type: 'add-job', payload: form })
+    }
+    const emptyForm = {
+      id: crypto.randomUUID(),
+      date: "",
+      name: "", 
+      address: "",
+      type: formJobType,
+      items: [],
+      status: "Draft",
+      details: false,
+      editing: false
+    }
+    setForm(emptyForm)
+    setQuoteForm(false)
+    setRooms([
+      {type: "Bedroom", name: "Bedroom"},
+      {type: "Kitchen", name: "Kitchen"},
+      {type: "Living Room", name: "Living Room"},
+    ])
+  }
+
+  function updateForm(e) {
+    switch (e.target.name) {
+      case 'date':
+        setForm(prev => ({ ...prev, date: e.target.value }))
+        break
+      case 'name':
+        setForm(prev => ({ ...prev, name: e.target.value }))
+        break
+      case 'address':
+        setForm(prev => ({ ...prev, address: e.target.value }))
+        break
+      case 'job-type':
+        setForm(prev => ({ ...prev, type: e.target.value, items: [...form.items] }))
+        setformJobType(e.target.value)
+        break
+      case 'Kitchen':
+      case 'Bedroom':
+      case 'Living Room':
+      case 'Bedroom 2':
+      case 'Bedroom 3': {
+        const index = form.items.findIndex(item => item.name === e.target.name)
+        if (index !== -1) {
+          setForm(prev => {
+            const updatedItems = prev.items.map((item, i) => 
+              i === index ? { ...item, description: e.target.value } : item
+            )
+            return { ...prev, items: updatedItems }
+          })
+        } else {
+            setForm(prev => ({
+              ...prev,
+              items: [...prev.items, { name: e.target.name, description: e.target.value }]
+            }))
+        }
+        break
+      }
+      case 'Kitchen-cc':
+      case 'Bedroom-cc':
+      case 'Living Room-cc':
+      case 'Bedroom 2-cc':
+      case 'Bedroom 3-cc': {
+        const room = e.target.name.slice(0, -3)
+        const index = form.items.findIndex(item => item.name === room)
+        if (index !== -1) {
+          setForm(prev => {
+            const updatedItems = prev.items.map((item, i) => 
+              i === index ? { ...item, cc: e.target.checked } : item
+            )
+            return { ...prev, items: updatedItems }
+          })
+        } else {
+            setForm(prev => ({
+              ...prev,
+              items: [...prev.items, { name: room, cc: e.target.checked }]
+            }))
+        }
+        break
+      }
+      case 'sqft': {
+        if (form.items.length !== 0) {
+            setForm(prev => ({ 
+            ...prev,
+            items: [{ ...prev.items[0], sqft: e.target.value }]
+          }))
+        } else {
+            setForm(prev => ({ 
+            ...prev,
+            items: [{ sqft: e.target.value }]
+          }))
+        }
+        break
+      }
+      case 'colors': {
+        if (form.items.length !== 0) {
+            setForm(prev => ({ 
+            ...prev,
+            items: [{ ...prev.items[0], colors: e.target.value }]
+          }))
+        } else {
+            setForm(prev => ({ 
+            ...prev,
+            items: [{ colors: e.target.value }]
+          }))
+        }
+        break
+      }
+      case 'exterior-color-change': {
+        if (form.items.length !== 0) {
+            setForm(prev => ({ 
+            ...prev,
+            items: [{ ...prev.items[0], cc: e.target.checked }]
+          }))
+        } else {
+            setForm(prev => ({ 
+            ...prev,
+            items: [{ cc: e.target.checked }]
+          }))
+        }
+        break
+      }
+      default:
+        return 
+    }
+  }
+
+  function jobEdit(id) {
+    let editingQuote = quotes.filter(quote => quote.id === id)[0]
+    editingQuote.editing = true
+    setForm(editingQuote)
+    setformJobType(editingQuote.type)
+    setQuoteForm(true)
+  }
+
+  function checkRoomSelection(type, name) {
+    let checked
+    form.items.map((item) => {
+      if (item.description === type && item.name === name) {
+        checked = true
+        return
+      }
+    })
+    return checked
+  }
+
+  function checkCC(room) {
+    let checked
+    form.items.map((item) => {
+      if (item.name === room) {
+        if (item.cc === true) {
+          checked = true
+        }
+      }
+    })
+    return checked
+  }
+
+  function updateStatus(quote) {
+    let selectedQuote = quotes.filter((q) => q.id === quote.id)
+    if (selectedQuote[0].status === "Draft") { selectedQuote[0].status = "Sent" }
+    else if (selectedQuote[0].status === "Sent") { selectedQuote[0].status = "Rejected" }
+    else if (selectedQuote[0].status === "Rejected") { selectedQuote[0].status = "Accepted" }
+    else if (selectedQuote[0].status === "Accepted") { selectedQuote[0].status = "Draft" }
+
+    dispatch({ type: 'status-update', payload: selectedQuote[0] })
+  }
+
+  return (
+    <div className='min-h-screen bg-gray-900 p-4 text-white'>
+      <div className='max-w-2x1 my-12 p-6'>
+        <h2 className='text-lg'>Quotes</h2>
+        <div className='mt-4 flex flex-col gap-4'>
+          <span className='grid grid-cols-7 text-sm p-4'>
+            <span>Date</span>
+            <span>Name</span>
+            <span>Address</span>
+            <span>Job Type</span>
+            <span>Quote Status</span>
+            <span>Total</span>
+          </span>
+          {quotes.map((quote) => (
+              <div className='grid grid-cols-7 border-2 items-center border-gray-600 bg-gray-700 rounded-2xl p-2 text-sm' key={quote.id}>
+                    <span>{quote.date}</span>
+                    <span>{quote.name}</span>
+                    <span>{quote.address}</span>
+                    <span>{quote.type}</span>
+                    <span 
+                      className={`border-2 rounded-xl w-24 text-center p-1 font-bold cursor-pointer
+                      ${quote.status === "Draft" && 'before:content-["📜"] border-yellow-700 bg-yellow-900'}
+                      ${quote.status === "Sent" && 'before:content-["📩"] border-blue-700 bg-blue-900'} 
+                      ${quote.status === "Rejected" && 'before:content-["❌"] border-red-700 bg-red-900'}
+                      ${quote.status === "Accepted" && 'before:content-["✅"] border-green-700 bg-green-900'}`}
+                      onClick={() => updateStatus(quote)}> {quote.status}
+                    </span>
+                    <span>{quote.total}</span>
+                    <div className='flex flex-wrap gap-2'>
+                      <button onClick={() => dispatch({ type: 'toggle-details', payload: quote.id })} className={`px-2 py-2 rounded-lg text-sm font-medium border bg-indigo-600 border-indigo-500 cursor-pointer`}>
+                        {quote.details ? "Hide Details" : "See Details"}
+                      </button>
+                      <button onClick={() => jobEdit(quote.id)} className={`px-4 py-2 rounded-lg text-sm font-medium border bg-indigo-600 border-indigo-500 cursor-pointer`}>
+                        Edit
+                      </button>
+                      <button onClick={() => dispatch({ type: 'remove-job', payload: quote.id })} className={`px-4 py-2 rounded-lg text-sm font-medium border bg-indigo-600 border-indigo-500 cursor-pointer`}>
+                        Remove
+                      </button>
+                    </div>
+                    { quote.details && 
+                      <ul className='flex flex-col gap-4 m-4'>
+                        {quote.items.map((entry, index) => {
+                          if (quote.type === "Interior") {
+                            return <li className='' key={index}>{ entry.description } { entry.description && entry.cc && "(Color Change)" }</li>
+                          } else if (quote.type === "Exterior") {
+                            return <li className='flex' key={index}>Sqft: { entry.sqft } Colors: { entry.colors } { entry.cc && "(Color Change)" }</li>
+                          }
+                        })}
+                      </ul>
+                    }
+              </div>
+          ))}
+        </div>
+        { quoteForm ? (
+          <>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-6 bg-gray-200 text-gray-900 p-6 mt-12 w-2/5 rounded-md'>
+              <h3 className='self-center mb-4'>Quote Form</h3>
+              <div className='flex items-center gap-4'>
+                <label for="date">Date:</label>
+                <input type="date" id="date" name="date" value={form.editing ? form.date : undefined} onChange={(e) => updateForm(e)} required className='bg-gray-400 p-2 rounded-lg'/>
+              </div>
+              <div className='flex items-center gap-4'>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" value={form.editing ? form.name : undefined} onChange={(e) => updateForm(e)} required className='bg-gray-400 p-2 rounded-lg'/>
+              </div>
+              <div className='flex items-center gap-4'>
+                <label for="address">Address:</label>
+                <input type="text" id="address" name="address" value={form.editing ? form.address : undefined} onChange={(e) => updateForm(e)} required className='bg-gray-400 p-2 rounded-lg'/>
+              </div>
+              <div className='flex items-center gap-4'>
+                <label for="job-type">Job type:</label>
+                <select 
+                  id="job-type"
+                  name='job-type'
+                  value={formJobType}
+                  onChange={(e) => updateForm(e)}
+                >
+                  <option value="Interior">Interior</option>
+                  <option value="Exterior">Exterior</option>
+                </select>
+              </div>
+              <div className='flex flex-col'>
+                { formJobType === 'Interior' && (
+                  <>
+                    { rooms.map((room) => (
+                      <div className='flex gap-2 mt-4'>
+                        <span>{room.name}:</span>
+                        <input type="radio" id={`${room.name}-walls`} name={room.name} onChange={(e) => updateForm(e)} value={`${room.type} (Walls)`} checked={form.editing && checkRoomSelection(`${room.type} (Walls)`, room.name,) ? true : undefined}/>
+                        <label for={`${room.name}-walls`}>Walls</label>
+                        <input type="radio" id={`${room.name}-walls-trim`} name={room.name} onChange={(e) => updateForm(e)} value={`${room.type} (Walls + Trim)`} checked={form.editing && checkRoomSelection(`${room.type} (Walls + Trim)`, room.name) ? true : undefined}/>
+                        <label for={`${room.name}-walls-trim`}>Walls + Trim</label>
+                        <input type="radio" id={`${room.name}-walls-trim-ceiling`} name={room.name} onChange={(e) => updateForm(e)} value={`${room.type} (Walls + Trim + Ceiling)`} checked={form.editing && checkRoomSelection(`${room.type} (Walls + Trim + Ceiling)`, room.name) ? true : undefined}/>
+                        <label for={`${room.name}-walls-trim-ceiling`}>Walls + Trim + Ceiling</label>
+                        <input type="checkbox" id={`${room.name}-color-change`} name={`${room.name}-cc`} onChange={(e) => updateForm(e)} value={`${room.type} Color Change`} checked={form.editing && checkCC(`${room.name}`) ? true : undefined}/>
+                        <label for={`${room.name}-color-change`}>Color Change</label> 
+                      </div>
+                    ))}
+                    { rooms.length < 5 && (
+                      <button
+                        className={`px-4 py-2 mt-8 rounded-lg text-sm text-gray-200 font-medium border bg-indigo-600 border-indigo-500 flex-1 w-1/3 cursor-pointer`}
+                        onClick={addBedroom}
+                      >
+                        Add Bedroom
+                      </button>
+                    )}
+                  </>
+                )}
+                { formJobType === 'Exterior' && (
+                  <>
+                    <div className='flex flex-col mt-4 gap-4'>
+                      <div className='flex items-center gap-2'>
+                        <label for="sqft">Sqft:</label>
+                        <input type="number" name="sqft" id="sqft" value={form.editing ? form.items[0].sqft : undefined} min="0" onChange={(e) => updateForm(e)} required className="bg-gray-400 p-2 rounded-lg"/>
+                      </div>
+                      <div className='flex gap-2'>
+                        <span>Colors:</span>
+                        <input type="radio" id="1-color" name="colors" onChange={(e) => updateForm(e)} value="1" checked={form.editing && form.items[0].colors === "1" ? true : undefined} required/>
+                        <label for="1-color">1</label>
+                        <input type="radio" id="2-color" name="colors" onChange={(e) => updateForm(e)} value="2" checked={form.editing && form.items[0].colors === "2" ? true : undefined} required/>
+                        <label for="2-color">2</label>
+                        <input type="radio" id="3-color" name="colors" onChange={(e) => updateForm(e)} value="3" checked={form.editing && form.items[0].colors === "3" ? true : undefined} required/>
+                        <label for="3-color">3</label>
+                      </div>
+                      <div className='flex gap-2'>
+                        <label for="exterior-color-change">Color Change</label> 
+                        <input type="checkbox" id="exterior-color-change" name="exterior-color-change" onChange={(e) => updateForm(e)} value="Exterior Color Change" checked={form.editing && form.items[0].cc === true ? true : undefined}/>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+              <div className='flex gap-2 text-gray-200'>
+                <button
+                  className={`px-4 py-2 mt-4 rounded-lg text-sm font-medium border bg-indigo-600 border-indigo-500 flex-1 w-1/3 cursor-pointer`}
+                  type='submit'
+                >
+                  Save
+                </button>
+                <button
+                  className={`px-4 py-2 mt-4 rounded-lg text-sm font-medium border bg-indigo-600 border-indigo-500 flex-1 w-1/3 cursor-pointer`}
+                  onClick={() => {
+                    setQuoteForm((prev) => !prev)
+                    setForm({
+                      id: crypto.randomUUID(),
+                      date: "",
+                      name: "", 
+                      address: "",
+                      type: formJobType,
+                      items: [],
+                      status: "Draft",
+                      total: getTotal(),
+                      details: false,
+                      editing: false
+                    })
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </>
+        ) : (
+          <button
+            className={`px-4 py-2 mt-8 rounded-lg text-sm font-medium border bg-indigo-600 border-indigo-500 flex-1 cursor-pointer`}
+            onClick={() => setQuoteForm((prev) => !prev)}
+          >
+            + Add Quote
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
